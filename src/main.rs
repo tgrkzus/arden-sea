5d130a1c849a3125f2a1ffaf69a14e96dd028817
Tom Richardson <tgrkzus@gmail.com>
2017-09-05 19:33:53 +1000

    movement!

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 extern crate tcod;
 
 use tcod::*;
+use tcod::input::*;
 
 fn main() {
 
@@ -9,11 +10,40 @@ fn main() {
     let mut x = 0;
     let mut y = 0;
     loop {
+        // Clear
+        root.clear();
+
+        // Draw state
         root.put_char_ex(x, y, 
                          '@', 
                          colors::YELLOW, 
                          colors::BLACK);
+        // Flush changes
         root.flush();
+        
+        // process input
+        let c = root.wait_for_keypress(false);
+
+        if c.code == KeyCode::Char {
+            match c.printable {
+                'A' | 'a' => x -= 1,
+                'D' | 'd' => x += 1,
+                'W' | 'w' => y -= 1,
+                'S' | 's' => y += 1,
+                _ => println!("Invalid input"),
+            }
+        }
+        else {
+            match c.code {
+                KeyCode::Left  => x -= 1,
+                KeyCode::Right => x += 1,
+                KeyCode::Up    => y -= 1,
+                KeyCode::Down  => y += 1,
+                _ => println!("Invalid input"),
+            }
+        }
+
+
     }
 
     cleanup_game();
