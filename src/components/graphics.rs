87b30c551de03b4b58b2383e8cebeb9ad5185391
Tom Richardson <tgrkzus@gmail.com>
2017-09-25 21:01:56 +1000

    Log frame window

--- src/components/graphics.rs
+++ src/components/graphics.rs
@@ -8,6 +8,10 @@ use components::position::*;
 use game::WorldAttributes;
 
 const WORLD_OFFSET: (i32, i32) = (10, 10);
+const WORLD_WINDOW_SIZE: (i32, i32) = (80, 80);
+
+const LOG_OFFSET: (i32, i32) = (92, 10);
+const LOG_SIZE: (i32, i32) = (60, 80);
 
 #[derive(Debug)]
 pub struct CharacterRenderComponent {
@@ -32,27 +36,38 @@ impl<'a> System<'a> for RenderSystem {
         window.clear();
 
         // Make offscreens
-        let mut world_screen = Offscreen::new(attrs.size.0 as i32, attrs.size.1 as i32);
+        let mut world_screen = Offscreen::new(WORLD_WINDOW_SIZE.0, WORLD_WINDOW_SIZE.1);
+        let mut log_screen = Offscreen::new(LOG_SIZE.0, LOG_SIZE.1);
 
         // Render characters
         for (render, position) in (&render, &position).join() {
             world_screen.put_char_ex(position.x, position.y, render.c, colors::RED, colors::BLACK);
         }
 
-        RenderSystem::draw_frame(&mut *window, WORLD_OFFSET.0 - 1, WORLD_OFFSET.1 - 1, attrs.size.0 + 1, attrs.size.1 + 1, colors::DESATURATED_FLAME);
-
-        tcod::console::blit(&world_screen, (0, 0), (attrs.size.0 as i32, attrs.size.0 as i32),
+        // World window frame + blitting
+        RenderSystem::draw_frame(&mut *window, WORLD_OFFSET.0 - 1, WORLD_OFFSET.1 - 1, WORLD_WINDOW_SIZE.0 + 1, WORLD_WINDOW_SIZE.1 + 1, colors::DESATURATED_FLAME);
+        tcod::console::blit(&world_screen, (0, 0), WORLD_WINDOW_SIZE,
                       &mut (*window), WORLD_OFFSET, 1.0, 1.0);
+
+
+        // Write to log
+        log_screen.print_rect(0, 0, LOG_SIZE.0, LOG_SIZE.1,
+                              "Hey some text is here!\nWith a new line!");
+
+        // Log window frame + blitting
+        RenderSystem::draw_frame(&mut *window, LOG_OFFSET.0 - 1, LOG_OFFSET.1 - 1, LOG_SIZE.0 + 1, LOG_SIZE.1 + 1, colors::DESATURATED_FLAME);
+        tcod::console::blit(&log_screen, (0, 0), LOG_SIZE,
+                      &mut (*window), LOG_OFFSET, 1.0, 1.0);
         // Flush changes
         window.flush();
     }
 }
 
 impl RenderSystem {
-
     fn draw_frame(console: &mut Console, x: i32, y: i32, width: i32, height: i32, color: colors::Color) {
         console.set_default_foreground(color);
         console.horizontal_line(x, y, width, BackgroundFlag::None);
+        println!("{}, {}, {}, {}", x, y, width, height);
         console.horizontal_line(x, y + height, width, BackgroundFlag::None);
 
         console.vertical_line(x, y, height, BackgroundFlag::None);
